72c2d0530b0c7143e25935db0c35cf4d075de202
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-04 22:25:53 -0300

    defines a graph producer

---
+++ careerbuddy/src/main/java/br/com/eldermoraes/careerbuddy/driver/GraphProducer.java
@@ -0,0 +1,46 @@
+package br.com.eldermoraes.careerbuddy.driver;
+
+import com.steelbridgelabs.oss.neo4j.structure.Neo4JElementIdProvider;
+import com.steelbridgelabs.oss.neo4j.structure.Neo4JGraph;
+import com.steelbridgelabs.oss.neo4j.structure.providers.Neo4JNativeElementIdProvider;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.jnosql.artemis.ConfigurationUnit;
+import org.neo4j.driver.v1.Driver;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+public class GraphProducer {
+
+    private Neo4JGraph graph;
+
+    @Inject
+    @ConfigurationUnit
+    private Driver driver;
+
+
+
+    @PostConstruct
+    public void init() {
+        Neo4JElementIdProvider<?> vertexIdProvider = new Neo4JNativeElementIdProvider();
+        Neo4JElementIdProvider<?> edgeIdProvider = new Neo4JNativeElementIdProvider();
+        this.graph = new Neo4JGraph(driver, vertexIdProvider, edgeIdProvider);
+        graph.setProfilerEnabled(true);
+    }
+
+
+    @Produces
+    @ApplicationScoped
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public void close(@Disposes Graph graph) throws Exception {
+        graph.close();
+        driver.close();
+    }
+
+}
