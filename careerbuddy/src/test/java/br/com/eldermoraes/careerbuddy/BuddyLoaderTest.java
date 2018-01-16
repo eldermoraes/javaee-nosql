d216bcd021f4d969927e0b67e94fc3a3cd5dbd3a
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-15 22:27:10 -0200

    creates a CDI extensions

--- careerbuddy/src/test/java/br/com/eldermoraes/careerbuddy/BuddyLoaderTest.java
+++ careerbuddy/src/test/java/br/com/eldermoraes/careerbuddy/BuddyLoaderTest.java
@@ -1,7 +1,20 @@
 package br.com.eldermoraes.careerbuddy;
 
+import br.com.eldermoraes.careerbuddy.cdi.CDIExtension;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.inject.Inject;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BuddyLoaderTest {
+@ExtendWith(CDIExtension.class)
+public class BuddyLoaderTest {
 
+
+    @Inject
+    private BuddyLoader buddyLoader;
+
+    @Inject
+    private Graph graph;
 }
--- careerbuddy/src/test/java/br/com/eldermoraes/careerbuddy/driver/EmbeddedSupplier.java
+++ careerbuddy/src/test/java/br/com/eldermoraes/careerbuddy/driver/EmbeddedSupplier.java
@@ -1,10 +1,23 @@
 package br.com.eldermoraes.careerbuddy.driver;
 
+import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.interceptor.Interceptor;
+import java.io.File;
+
+@ApplicationScoped
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION+10)
 public class EmbeddedSupplier implements GraphSupplier {
+
+    private Graph graph = Neo4jGraph.open(new File("").getAbsolutePath() + "/target/jnosql-graph");
+
     @Override
     public Graph get() {
-        return null;
+        return graph;
     }
 }
