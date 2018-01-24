3c1c2ff0a083e8f7755a67c41506da8e16e10ff6
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-24 19:41:19 -0200

    adds technology script

--- careerbuddy/load.sh
+++ careerbuddy/load.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+#cities
 curl -H "Content-Type: application/json" -X POST -d 'Sao Paulo' http://localhost:8080/careerbuddy/resource/cities/
 curl -H "Content-Type: application/json" -X POST -d 'Salvador' http://localhost:8080/careerbuddy/resource/cities/
 curl -H "Content-Type: application/json" -X POST -d 'Belo Horizonte' http://localhost:8080/careerbuddy/resource/cities/
 curl -H "Content-Type: application/json" -X POST -d 'Rio de Janeiro' http://localhost:8080/careerbuddy/resource/cities/
 curl -H "Content-Type: application/json" -X POST -d 'Curitiba' http://localhost:8080/careerbuddy/resource/cities/
+
+#technologies
+curl -H "Content-Type: application/json" -X POST -d 'Java' http://localhost:8080/careerbuddy/resource/technologies/
+curl -H "Content-Type: application/json" -X POST -d 'NoSQL' http://localhost:8080/careerbuddy/resource/technologies/
+curl -H "Content-Type: application/json" -X POST -d 'Cloud' http://localhost:8080/careerbuddy/resource/technologies/
+curl -H "Content-Type: application/json" -X POST -d 'Container' http://localhost:8080/careerbuddy/resource/technologies/
+curl -H "Content-Type: application/json" -X POST -d 'Go' http://localhost:8080/careerbuddy/resource/technologies/
