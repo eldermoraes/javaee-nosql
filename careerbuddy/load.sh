39e8b0506e7c82308c323f5969be371bbd2835bb
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-24 19:36:54 -0200

    create city script

---
+++ careerbuddy/load.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -H "Content-Type: application/json" -X POST -d 'Sao Paulo' http://localhost:8080/careerbuddy/resource/cities/
+curl -H "Content-Type: application/json" -X POST -d 'Salvador' http://localhost:8080/careerbuddy/resource/cities/
+curl -H "Content-Type: application/json" -X POST -d 'Belo Horizonte' http://localhost:8080/careerbuddy/resource/cities/
+curl -H "Content-Type: application/json" -X POST -d 'Rio de Janeiro' http://localhost:8080/careerbuddy/resource/cities/
+curl -H "Content-Type: application/json" -X POST -d 'Curitiba' http://localhost:8080/careerbuddy/resource/cities/
