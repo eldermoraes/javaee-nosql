f620c63b8b6d566dc7648546ac6ce9dcd5f16ed5
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-24 20:09:53 -0200

    adds lives resource

--- careerbuddy/load.sh
+++ careerbuddy/load.sh
@@ -21,3 +21,10 @@ curl -H "Content-Type: application/json" -X POST -d '{"name":"Mario","salary":50
 curl -H "Content-Type: application/json" -X POST -d '{"name":"Joao","salary":9000.0}' http://localhost:8080/careerbuddy/resource/buddies/
 curl -H "Content-Type: application/json" -X POST -d '{"name":"Pedro","salary":14000.0}' http://localhost:8080/careerbuddy/resource/buddies/
 
+#lives
+
+curl -H "Content-Type: application/json" -X PUT http://localhost:8080/careerbuddy/resource/buddies/mario/lives/salvador
+curl -H "Content-Type: application/json" -X PUT http://localhost:8080/careerbuddy/resource/buddies/joao/lives/belohorizonte
+curl -H "Content-Type: application/json" -X PUT http://localhost:8080/careerbuddy/resource/buddies/pedro/lives/saopaulo
+curl -H "Content-Type: application/json" -X PUT http://localhost:8080/careerbuddy/resource/buddies/jose/lives/saopaulo
+
