b2b7ce85de04408004c42b1a38e98a25d06d43bf
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-25 08:04:37 -0200

    defines producer as application|

--- careerbuddy/src/main/java/br/com/eldermoraes/careerbuddy/driver/GraphProducer.java
+++ careerbuddy/src/main/java/br/com/eldermoraes/careerbuddy/driver/GraphProducer.java
@@ -18,11 +18,13 @@ package br.com.eldermoraes.careerbuddy.driver;
 
 import org.apache.tinkerpop.gremlin.structure.Graph;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+@ApplicationScoped
 public class GraphProducer {
 
     @Inject
