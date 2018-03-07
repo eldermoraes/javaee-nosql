3759bd4598b2357c873716227d949243f49fa0e2
Otavio Santana <otaviopolianasantana@gmail.com>
2018-03-07 12:04:46 -0300

    improves message

--- careerbuddy/src/main/java/br/com/eldermoraes/careerbuddy/CityResource.java
+++ careerbuddy/src/main/java/br/com/eldermoraes/careerbuddy/CityResource.java
@@ -31,11 +31,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.status;
 import static org.jnosql.artemis.DatabaseType.GRAPH;
 
 @ApplicationScoped
@@ -50,12 +53,17 @@ public class CityResource {
     @Database(GRAPH)
     private CityRepository cityRepository;
 
+    @Inject
+    private CacheControl cacheControl;
+
 
     @POST
     public void insert(@Valid @Name String name) {
 
         cityRepository.findByName(br.com.eldermoraes.careerbuddy.Name.of(name).get()).ifPresent(b -> {
-            throw new WebApplicationException("There is city that already does exist", Response.Status.BAD_REQUEST);
+            String message = "There is city that already does exist with this name: " + name;
+            throw new WebApplicationException(message, status(BAD_REQUEST)
+                    .entity(message).cacheControl(cacheControl).build());
         });
 
         cityRepository.save(new City(name));
@@ -65,7 +73,11 @@ public class CityResource {
     @Path("{name}")
     public CityDTO get(@PathParam("name") String name) {
         City city = cityRepository.findByName(br.com.eldermoraes.careerbuddy.Name.of(name).get())
-                .orElseThrow(() -> new WebApplicationException("city does not found", Response.Status.NOT_FOUND));
+                .orElseThrow(() -> {
+                    String message = "city does not found name: " + name;
+                    return new WebApplicationException(message, status(BAD_REQUEST)
+                            .entity(message).build());
+                });
 
         return new CityDTO(city);
     }
